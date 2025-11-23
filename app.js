c221aee738458d933413914b7d232184fef5e7e6
joshonline <masanzajoshua@gmail.com>
2025-11-23 04:05:08 -0500

    Created and configured developer database and connection

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var expressLayouts = require("express-ejs-layouts");
+var connectDB = require("./config/db");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const expressEjsLayouts = require("express-ejs-layouts");
 
 var app = express();
+
+connectDB();
 
 // view engine setup
 app.use(expressEjsLayouts);
