fcd42d8b13087a70f08d7397f2fb52ac3e448271
joshonline <masanzajoshua@gmail.com>
2025-11-23 19:56:20 -0500

    Added routes for the app

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET /users/ - redirect dashboard if logged in
+router.get("/", (req, res) => {
+  if (req.session && req.session.userId) {
+    return res.redirect("/bookmarks/list");
+  }
+  res.redirect("/users/login");
 });
+
+// GET /users/signup
+router.get("/signup", userController.getSignup);
+
+// POST /users/signup
+router.post("/signup", userController.postSignup);
+
+// GET /users/login
+router.get("/login", userController.getLogin);
+
+// POST /users/login
+router.post("/login", userController.postLogin);
+
+// POST /users/logout
+router.post("/logout", userController.logout);
+
+// GET /users/profile
+router.get("/profile", userController.getProfile);
+
+// POST /users/profile
+router.post("/profile", userController.postProfile);
 
 module.exports = router;
